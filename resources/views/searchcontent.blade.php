6001e196c814cbf970ff361117226f35c76da1ad
nemeskolos <kolos100@gmail.com>
2019-11-23 11:38:31 +0100

    11.23/2

--- resources/views/searchcontent.blade.php
+++ resources/views/searchcontent.blade.php
@@ -33,6 +33,10 @@
 </div>
 </div>
 
-
+<div class="jumbotron text-center" style="margin-bottom:0">
+      <p class="post-info">2019 © SZTE <br>
+        Info-Bionika || Rendszerfejlesztés
+      </p>
+    </div>
 
 @endsection
---
+++ resources/views/sucadvmsg.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+<title>Sikeres</title>
+@section('content')
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+</head>
+<body>
+   <!--ide írd a kódod-->
+</body>
+</html>
+
+<div class="jumbotron text-center" style="margin-bottom:0">
+      <p class="post-info">2019 © SZTE <br>
+        Info-Bionika || Rendszerfejlesztés
+      </p>
+    </div>
+
+@endsection
