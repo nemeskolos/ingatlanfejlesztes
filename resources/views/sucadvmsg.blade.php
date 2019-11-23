7f70f7a1b7aee7a61eb16d7efa02bbf1cea23133
nemeskolos <kolos100@gmail.com>
2019-11-23 11:41:08 +0100

    11.23/3

--- resources/views/sucadvmsg.blade.php
+++ resources/views/sucadvmsg.blade.php
@@ -4,21 +4,27 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/style/style.css">
+
+
 </head>
+
 <body>
-   <!--ide írd a kódod-->
+    <!--ide írd a kódod-->
 </body>
+
 </html>
 
 <div class="jumbotron text-center" style="margin-bottom:0">
-      <p class="post-info">2019 © SZTE <br>
+    <p class="post-info">2019 © SZTE <br>
         Info-Bionika || Rendszerfejlesztés
-      </p>
-    </div>
+    </p>
+</div>
 
 @endsection
